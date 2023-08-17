a0abebe247bc21a1dcc6817af339f3e939fc1f5b
Jeffreytsai1004 <109943015+Jeffreytsai1004@users.noreply.github.com>
2023-08-17 22:58:27 +0800

    Update activate.bat

--- activate.bat
+++ activate.bat
@@ -1,3 +1,4 @@
-@CALL set VENV_NAME=VENV_SD_WEUI_v1.5.1_SDXL_001
+@CALL set WEUI_VERSION=v1.5.1
+@CALL set VENV_NAME=VENV_SD_WEUI_%WEUI_VERSION%_SDXL_001
 @CALL "%~dp0micromamba.exe" shell init --shell=cmd.exe --prefix="%~dp0\"
 start cmd /k "%~dp0condabin\micromamba.bat" activate %VENV_NAME%
