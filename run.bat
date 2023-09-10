400e754142f495f21555f37fde34a2bedd9493ec
Jeffreytsai1004 <109943015+Jeffreytsai1004@users.noreply.github.com>
2023-09-11 03:29:37 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,8 @@
 @CALL echo off
-@CALL set WEUI_VERSION=v1.5.1
+
+@CALL echo ---------------------------------------------------------------------------------------
+@CALL echo Setting VARIABLES
+@CALL set WEUI_VERSION=v1.6.0
 @CALL set VENV_NAME=VENV_SD_WEUI_%WEUI_VERSION%_SDXL_001
 @CALL set PYTHON_VERISON=3.10.11
 @CALL set GIT_VERSION=2.41.0
@@ -9,14 +12,16 @@
 @CALL set TORCH_HOME=cache\torch
 @CALL set HF_HOME=cache\huggingface
 @CALL set PYTHONDONTWRITEBYTECODE=1
-@CALL set COMMANDLINE_ARGS=--xformers --opt-sdp-attention --theme dark --api --autolaunch --listen --enable-insecure-extension-access
+@CALL set COMMANDLINE_ARGS=--theme dark --xformers --opt-sdp-attention --api --autolaunch --listen --enable-insecure-extension-access
+@CALL echo VARIABLES setted
+
 @CALL echo "-------------------------------------------------------------------------------------"
 @CALL echo "          ___ _        _    _         ___  _  __  __         _            _          "
 @CALL echo "         / __| |_ __ _| |__| |___ ___|   \(_)/ _|/ _|_  _ __(_)___ _ _   | |         "
 @CALL echo "         \__ \  _/ _` | '_ \ / -_)___| |) | |  _|  _| || (_-< / _ \ ' \  |_|         "
 @CALL echo "         |___/\__\__,_|_.__/_\___|   |___/|_|_| |_|  \_,_/__/_\___/_||_| (_)         "
 @CALL echo "-------------------------------------------------------------------------------------"
-@CALL echo LAUNCHING STABLE-DIFFUSION !!!!!
+@CALL echo RUN STABLE-DIFFUSION !!!!!
 @CALL echo ---------------------------------------------------------------------------------------
 @CALL echo LAUNCH ARGS:
 @CALL echo   WEUI_VERSION:                 %WEUI_VERSION%
@@ -29,9 +34,15 @@
 @CALL echo   HF_HOME:                      %HF_HOME%
 @CALL echo   PYTHONDONTWRITEBYTECODE:      %PYTHONDONTWRITEBYTECODE%
 @CALL echo   COMMANDLINE_ARGS:             %COMMANDLINE_ARGS%
+
 @CALL echo ---------------------------------------------------------------------------------------
+@CALL echo Activating %VENV_NAME%
 @CALL "%~dp0micromamba.exe" shell init --shell=cmd.exe --prefix="%~dp0\"
+@CALL echo %VENV_NAME% activated
+
+@CALL echo ---------------------------------------------------------------------------------------
 @CALL condabin\micromamba.bat activate %VENV_NAME%
 @CALL cd stable-diffusion-webui
 @CALL python -B webui.py %COMMANDLINE_ARGS%
+
 @CALL PAUSE
