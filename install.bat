a1d336e37e2af6d4d48e6aaedd3367e4738fa145
Jeffreytsai1004 <109943015+Jeffreytsai1004@users.noreply.github.com>
2023-12-11 21:50:04 +0800

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,11 +1,11 @@
-@CALL "%~dp0micromamba.exe" create -n stable-diffusion-webui-001 python==3.10.11 git git-lfs -c pytorch -c conda-forge -r "%~dp0\" -y
+@CALL "%~dp0micromamba.exe" create -n stable-diffusion-webui-001 python==3.10.11 git==2.41.0 git-lfs==3.2.0 -c pytorch -c conda-forge -r "%~dp0\" -y
 @CALL "%~dp0micromamba.exe" shell init --shell cmd.exe --prefix "%~dp0\"
 @CALL condabin\micromamba.bat activate stable-diffusion-webui-001
 @CALL set GDOWN_CACHE=cache\gdown
 @CALL set TORCH_HOME=cache\torch
 @CALL set HF_HOME=cache\huggingface
 @CALL set PYTHONDONTWRITEBYTECODE=1
-@CALL pip install xformer==0.0.21 torch=2.0.1 torchvision torchaudio torchtext PySoundFile
+@CALL pip install xformer==0.0.20 torch==2.0.1+cu118 torchvision torchaudio torchtext PySoundFile
 @CALL pip install -r requirements.txt
 @CALL git clone -b v1.7.0-RC https://github.com/AUTOMATIC1111/stable-diffusion-webui/
 @CALL mkdir stable-diffusion-webui\cache\gdown\
