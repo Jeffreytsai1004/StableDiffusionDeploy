1c28ced304a5d4d295e4ce2637da29c5fbc96ff5
Jeffreytsai1004 <109943015+Jeffreytsai1004@users.noreply.github.com>
2024-06-05 22:50:41 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -4,7 +4,7 @@
 @CALL set TORCH_HOME=cache\torch
 @CALL set HF_HOME=cache\huggingface
 @CALL set PYTHONDONTWRITEBYTECODE=1
-@CALL set TF_ENABLE_ONEDNN_OPTS=0
+@CALL set CUDA_HOME=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.1
 @CALL set COMMANDLINE_ARGS=--listen --port 7860 --autolaunch --theme dark --xformers --opt-sdp-attention --api --skip-install --skip-torch-cuda-test --skip-version-check --enable-insecure-extension-access
 @CALL cd stable-diffusion-webui
 @CALL python -B webui.py %COMMANDLINE_ARGS%
