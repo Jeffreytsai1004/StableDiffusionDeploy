1e434e4fad15b9ac88e0b64a6b1256a609c3c3ad
Jeffreytsai1004 <109943015+Jeffreytsai1004@users.noreply.github.com>
2024-06-05 22:47:27 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,10 +1,11 @@
 @CALL "%~dp0micromamba.exe" shell init --shell cmd.exe --prefix "%~dp0\"
-@CALL condabin\micromamba.bat activate stable-diffusion-webui-001
+@CALL condabin\micromamba.bat activate StableDiffusion_001
 @CALL set GDOWN_CACHE=cache\gdown
 @CALL set TORCH_HOME=cache\torch
 @CALL set HF_HOME=cache\huggingface
 @CALL set PYTHONDONTWRITEBYTECODE=1
-@CALL set COMMANDLINE_ARGS=--theme dark --xformers --opt-sdp-attention --autolaunch --listen --port 7860 --skip-torch-cuda-test --enable-insecure-extension-access
+@CALL set TF_ENABLE_ONEDNN_OPTS=0
+@CALL set COMMANDLINE_ARGS=--listen --port 7860 --autolaunch --theme dark --xformers --opt-sdp-attention --api --skip-install --skip-torch-cuda-test --skip-version-check --enable-insecure-extension-access
 @CALL cd stable-diffusion-webui
 @CALL python -B webui.py %COMMANDLINE_ARGS%
 @CALL PAUSE
