4c904be558e243f56c09791bc825c7d2ed89d3e2
Jeffreytsai1004 <109943015+Jeffreytsai1004@users.noreply.github.com>
2024-06-05 22:46:01 +0800

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,23 +1,31 @@
-@CALL "%~dp0micromamba.exe" create -n stable-diffusion-webui-001 python==3.10.11 git==2.41.0 git-lfs==3.2.0 -c pytorch -c conda-forge -r "%~dp0\" -y
+@CALL "%~dp0micromamba.exe" create -n StableDiffusion_001 python==3.10.14 git==2.41.0 git-lfs==3.2.0 -c pytorch -c conda-forge -r "%~dp0\" -y
 @CALL "%~dp0micromamba.exe" shell init --shell cmd.exe --prefix "%~dp0\"
-@CALL condabin\micromamba.bat activate stable-diffusion-webui-001
+@CALL condabin\micromamba.bat activate StableDiffusion_001
+@CALL pip install --force-reinstall torch==2.2.0+cu121 torchvision==0.17.0+cu121 torchaudio==2.2.0+cu121 --index-url https://download.pytorch.org/whl/cu121 --no-cache-dir
+@CALL git clone https://github.com/facebookresearch/xformers.git
+@CALL cd xformers
+@CALL git submodule update --init --recursive
+@CALL pip install -e .
+@CALL cd ..
+@CALL git clone https://github.com/NVlabs/nvdiffrast .\nvdiffrast
+@CALL cd nvdiffrast
+@CALL pip install .
+@CALL cd ..
 @CALL set GDOWN_CACHE=cache\gdown
 @CALL set TORCH_HOME=cache\torch
 @CALL set HF_HOME=cache\huggingface
 @CALL set PYTHONDONTWRITEBYTECODE=1
-@CALL pip install -r requirements.txt
-@CALL pip install torch==2.0.1+cu118 torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118 --force-reinstall --no-cache-dir
-@CALL pip install xformers==0.0.21 --force-reinstall --no-cache-dir
-@CALL git clone -b v1.7.0-RC https://github.com/AUTOMATIC1111/stable-diffusion-webui/
-@CALL mkdir stable-diffusion-webui\cache\gdown\
-@CALL mkdir stable-diffusion-webui\cache\torch\
-@CALL mkdir stable-diffusion-webui\cache\huggingface\
-@CALL mkdir stable-diffusion-webui\repositories\
-@CALL cd stable-diffusion-webui
-@CALL git clone https://github.com/CompVis/stable-diffusion.git repositories\stable-diffusion
-@CALL git clone https://github.com/CompVis/taming-transformers.git repositories\taming-transformers
-@CALL git clone https://github.com/crowsonkb/k-diffusion.git repositories\k-diffusion
-@CALL git clone https://github.com/sczhou/CodeFormer.git repositories\CodeFormer
-@CALL git clone http://github.com/salesforce/BLIP.git repositories\BLIP
-@CALL python -B launch.py --xformers --opt-sdp-attention --autolaunch --listen --port 7860 --skip-torch-cuda-test --enable-insecure-extension-access
+@CALL git clone -b v1.9.4 https://github.com/AUTOMATIC1111/stable-diffusion-webui/ .\stable-diffusion-webui\
+@CALL mkdir .\stable-diffusion-webui\cache\gdown\
+@CALL mkdir .\stable-diffusion-webui\cache\torch\
+@CALL mkdir .\stable-diffusion-webui\cache\huggingface\
+@CALL mkdir .\stable-diffusion-webui\repositories\
+@CALL git clone https://github.com/CompVis/stable-diffusion.git .\stable-diffusion-webui\repositories\stable-diffusion
+@CALL git clone https://github.com/CompVis/taming-transformers.git .\stable-diffusion-webui\repositories\taming-transformers
+@CALL git clone https://github.com/crowsonkb/k-diffusion.git .\stable-diffusion-webui\repositories\k-diffusion
+@CALL git clone https://github.com/sczhou/CodeFormer.git .\stable-diffusion-webui\repositories\CodeFormer
+@CALL git clone http://github.com/salesforce/BLIP.git .\stable-diffusion-webui\repositories\BLIP
+@CALL cd .\stable-diffusion-webui
+@CALL pip install -r .\stable-diffusion-webui\requirements.txt
+@CALL python -B launch.py --xformers --opt-sdp-attention --autolaunch --theme dark --listen --port 7860 --enable-insecure-extension-access --skip-torch-cuda-test
 @CALL PAUSE
